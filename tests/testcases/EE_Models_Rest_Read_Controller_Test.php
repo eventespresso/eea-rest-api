e66a67cb045af45c8388f1928a1297127e6c7f3e
Michael Nelson <michael@eventespresso.com>
2015-04-03 16:51:36 -0700

    refactored in preparation for having multiple controllers; specifically another for Writing data about models, and another one or two for config-data

--- tests/testcases/EE_Models_Rest_Read_Controller_Test.php
+++ tests/testcases/EE_Models_Rest_Read_Controller_Test.php
@@ -5,49 +5,49 @@ if ( !defined( 'EVENT_ESPRESSO_VERSION' ) ) {
 
 /**
  *
- * EED_REST_API_Test
+ * EE_Models_Rest_Read_Controller_Test
  *
  * @package			Event Espresso
  * @subpackage
  * @author				Mike Nelson
  *
  */
-class EED_REST_API_Test extends EE_UnitTestCase{
+class EE_Models_Rest_Read_Controller_Test extends EE_UnitTestCase{
 	public function test_extract_includes_for_this_model__basic(){
 		$this->assertEquals( array(
 			'EVT_ID',
 			'EVT_name'
-		), EED_REST_API::extract_includes_for_this_model( 'EVT_ID,EVT_name' ) );
+		), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( 'EVT_ID,EVT_name' ) );
 	}
 	public function test_extract_includes_for_this_model__extra_whitespace() {
 		$this->assertEquals( array(
 			'EVT_ID',
 			'EVT_name',
 			'EVT_desc'
-		), EED_REST_API::extract_includes_for_this_model( 'EVT_ID , EVT_name , EVT_desc' ) );
+		), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( 'EVT_ID , EVT_name , EVT_desc' ) );
 	}
 	public function test_extract_includes_for_this_model__related_model() {
-		$this->assertEquals( array(), EED_REST_API::extract_includes_for_this_model( 'Registration.*' ) );
+		$this->assertEquals( array(), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( 'Registration.*' ) );
 	}
 	public function test_extract_includes_for_this_model__related_model_all() {
 		$this->assertEquals( array(
 			'*'
-		), EED_REST_API::extract_includes_for_this_model( 'Registration.*', 'Registration' ) );
+		), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( 'Registration.*', 'Registration' ) );
 	}
 	public function test_extract_includes_for_this_model__related_models_but_searching_for_this_one() {
 		$this->assertEquals( array(
-		), EED_REST_API::extract_includes_for_this_model( 'Registration.REG_ID, Registration.Attendee.ATT_ID' ) );
+		), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( 'Registration.REG_ID, Registration.Attendee.ATT_ID' ) );
 	}
 	public function test_extract_includes_for_this_model__related_models_but_searching_for_other() {
 		$this->assertEquals( array(
 			'REG_ID',
 			'Attendee.ATT_ID'
-		), EED_REST_API::extract_includes_for_this_model( 'Registration.REG_ID, Registration.Attendee.ATT_ID', 'Registration' ) );
+		), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( 'Registration.REG_ID, Registration.Attendee.ATT_ID', 'Registration' ) );
 	}
 
 	public function test_handle_request_get_one__event_includes() {
 		$event = $this->new_model_obj_with_dependencies( 'Event' );
-		$result = EED_REST_API::handle_request_get_one( EED_REST_API::ee_api_namespace . 'events/' . $event->ID(), $event->ID(), 'EVT_ID,EVT_name' );
+		$result = EE_Models_Rest_Read_Controller::handle_request_get_one( EED_REST_API::ee_api_namespace . 'events/' . $event->ID(), $event->ID(), 'EVT_ID,EVT_name' );
 		$this->assertEquals(
 			array (
 				'EVT_ID' => $event->ID(),
@@ -56,7 +56,7 @@ class EED_REST_API_Test extends EE_UnitTestCase{
 	}
 	public function test_handle_request_get_one__event_include_non_model_field() {
 		$event = $this->new_model_obj_with_dependencies( 'Event' );
-		$result = EED_REST_API::handle_request_get_one( EED_REST_API::ee_api_namespace . 'events/' . $event->ID(), $event->ID(), 'EVT_desc_raw, EVT_desc' );
+		$result = EE_Models_Rest_Read_Controller::handle_request_get_one( EED_REST_API::ee_api_namespace . 'events/' . $event->ID(), $event->ID(), 'EVT_desc_raw, EVT_desc' );
 		$this->assertEquals(
 			array (
 				'EVT_ID' => $event->ID(),
@@ -65,11 +65,11 @@ class EED_REST_API_Test extends EE_UnitTestCase{
 				), $result );
 	}
 	public function test_extract_includes_for_this_model__null() {
-		$this->assertEquals( array(), EED_REST_API::extract_includes_for_this_model( '*' ) );
+		$this->assertEquals( array(), EE_Models_Rest_Read_Controller::extract_includes_for_this_model( '*' ) );
 	}
 	public function test_handle_request_get_one__event() {
 		$event = $this->new_model_obj_with_dependencies( 'Event' );
-		$result = EED_REST_API::handle_request_get_one( EED_REST_API::ee_api_namespace . 'events/' . $event->ID(), $event->ID() );
+		$result = EE_Models_Rest_Read_Controller::handle_request_get_one( EED_REST_API::ee_api_namespace . 'events/' . $event->ID(), $event->ID() );
 		unset( $result[ 'EVT_created' ] );
 		unset( $result[ 'EVT_modified' ] );
 		unset( $result[ 'EVT_visible_on' ] );
@@ -106,14 +106,14 @@ class EED_REST_API_Test extends EE_UnitTestCase{
 
 	public function test_handle_request_get_one__registration_include_attendee(){
 		$r = $this->new_model_obj_with_dependencies( 'Registration' );
-		$entity = EED_REST_API::handle_request_get_one( EED_REST_API::ee_api_namespace . 'registrations/' . $r->ID(), $r->ID(), 'Attendee.*');
+		$entity = EE_Models_Rest_Read_Controller::handle_request_get_one( EED_REST_API::ee_api_namespace . 'registrations/' . $r->ID(), $r->ID(), 'Attendee.*');
 		$this->assertArrayHasKey( 'attendee', $entity );
 	}
 
 	public function test_handle_request_get_one__registration_include_answers_and_questions(){
 		$r = $this->new_model_obj_with_dependencies( 'Registration' );
 		$a = $this->new_model_obj_with_dependencies( 'Answer', array( 'REG_ID' => $r->ID() ) );
-		$entity = EED_REST_API::handle_request_get_one( EED_REST_API::ee_api_namespace . 'registrations/' . $r->ID(), $r->ID(), 'Answer.Question.*');
+		$entity = EE_Models_Rest_Read_Controller::handle_request_get_one( EED_REST_API::ee_api_namespace . 'registrations/' . $r->ID(), $r->ID(), 'Answer.Question.*');
 		$this->assertArrayHasKey( 'answers', $entity );
 		$answers = $entity['answers'];
 		foreach( $answers as $answer ) {
@@ -124,7 +124,7 @@ class EED_REST_API_Test extends EE_UnitTestCase{
 	public function test_handle_request_get_one__registration_include_answers_and_question_bare_min_from_each(){
 		$r = $this->new_model_obj_with_dependencies( 'Registration' );
 		$a = $this->new_model_obj_with_dependencies( 'Answer', array( 'REG_ID' => $r->ID() ) );
-		$entity = EED_REST_API::handle_request_get_one( EED_REST_API::ee_api_namespace . 'registrations/' . $r->ID(), $r->ID(), 'Answer.ATT_ID, Answer.Question.QST_ID');
+		$entity = EE_Models_Rest_Read_Controller::handle_request_get_one( EED_REST_API::ee_api_namespace . 'registrations/' . $r->ID(), $r->ID(), 'Answer.ATT_ID, Answer.Question.QST_ID');
 		$this->assertArrayHasKey( 'answers', $entity );
 		$answers = $entity['answers'];
 		foreach( $answers as $answer ){
@@ -134,4 +134,4 @@ class EED_REST_API_Test extends EE_UnitTestCase{
 
 }
 
-// End of file EED_REST_API_Test.php
+// End of file EE_Models_Rest_Read_Controller_Test.php
